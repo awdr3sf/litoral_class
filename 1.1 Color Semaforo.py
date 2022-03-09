3c88481f020fbd7736d1fa5db74429067245aad4
awdr3sf <87214633+awdr3sf@users.noreply.github.com>
2022-03-09 14:02:30 -0500

    10.1

---
+++ 1.1 Color Semaforo.py
@@ -0,0 +1,8 @@
+v_color = input( "Digitar valor ")
+v_color_min = v_color.lower()
+if v_color_min == "verde":
+    print("Puede seguir")
+elif v_color_min == "amarillo":
+    print("Puede seguir pero con cuidado")
+elif v_color_min == "Rojo":
+    print("Detente weyyy")
