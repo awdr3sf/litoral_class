a8dbfbb83fd71426daf92abf2c5768b9bd619d1a
awdr3sf <87214633+awdr3sf@users.noreply.github.com>
2022-04-02 10:20:36 -0500

    Update 1.5 Examen Funciones Ejercicios.py

--- Ejercicios Python/1.5 Examen Funciones Ejercicios.py
+++ Ejercicios Python/1.5 Examen Funciones Ejercicios.py
@@ -15,32 +15,33 @@ def imc():
 
 def notas():
     nota1 = float(input("Digitar primera nota : "))
-    nota2 = float(input("Digitar primera nota : "))
-    nota3 = float(input("Digitar primera nota : "))
-    nota4 = float(input("Digitar primera nota : "))
+    nota2 = float(input("Digitar segunda nota : "))
+    nota3 = float(input("Digitar tercera nota : "))
+    nota4 = float(input("Digitar cuarta nota : "))
     promedio = (nota1+nota2+nota3+nota4) / 4
-    print("tu promedio es de : ",promedio)
-    if promedio >0 and promedio <2.9:
-        print("Usted reporobo con un promedio de : ",round(promedio,2))
-    elif promedio == 3 and promedio < 3.5:
-        print("Usted paso con un promedio regular de : ",round(promedio,2))
-    elif promedio == 3.6 and promedio < 4.0:
-        print("Usted paso con un promedio bueno de : ",round(promedio,2))
-    elif promedio == 4.1 and promedio < 4.5:
-        print("Usted paso con un promedio Muy Bueno de : ",round(promedio,2))
-    elif promedio == 4.6 and promedio < 5.0:
-        print("Usted paso con un promedio excelente de : ",round(promedio,2))
+    promedio1 = round(promedio, 2)
+    if promedio1 >0 and promedio1 <2.9:
+        print("Usted reporobo con un promedio de : ",promedio1)
+    elif promedio1 <= 3 and promedio1 < 3.5:
+        print("Usted paso con un promedio regular de : ",promedio1)
+    elif promedio1 <= 3.6 and promedio1 < 4.0:
+        print("Usted paso con un promedio bueno de : ",promedio1)
+    elif promedio1 <= 4.1 and promedio1 < 4.5:
+        print("Usted paso con un promedio Muy Bueno de : ",promedio1)
+    elif promedio1 <= 4.6 and promedio1 < 5.0:
+        print("Usted paso con un promedio excelente de : ",promedio1)
 
 def resultadoentero():
     numero1 = int(input("Digitar primer numero : "))
     numero2 = int(input("Digitar primer numero : "))
     resultado = numero1//numero2
     resultado2 = numero1/numero2
-    
+    print("El numero entero de divisor es : ",resultado, " y el numero total es de : ",resultado2)
 
-    print("El numero entero de divisor es : ",resultado, "y el numero total es de : ",resultado2)
-
-
+#valorx()
+#img()
+notas()
+#resultadoentero()
 
 
 
