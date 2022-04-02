e10f2a81a3e40d73f363687b7ab47a1c7b86ab22
awdr3sf <87214633+awdr3sf@users.noreply.github.com>
2022-04-02 16:42:09 -0500

    Update 1.3 Menu Listado While.py

--- Ejercicios en Clase/Python/Andres Camacho/1.3 Menu Listado While.py
+++ Ejercicios en Clase/Python/Andres Camacho/1.3 Menu Listado While.py
@@ -1,25 +1,28 @@
 #FUNCION DEFINIDA
-def menu0():
+class menu0:
     print("\n Menu Opciones")
     Menu1 = ['1. Menu Principal','2.Menu Personas','3.Menu Clientes','4.Salir']
     print("\n",Menu1[0],"\n",Menu1[1],"\n",Menu1[2],"\n",Menu1[3])
 #FUNCION LLAMADA
-menu0()
+menu = menu0
+
 #VARIABLE VALOR USUARIO
 Menu = int(input("Digitar un valor : "))
 #CONDICIONES PARA MOSTRAR MENU
 while Menu !=4 and Menu < 4:
     if Menu == 1:
         print("|=======Abriste Menu Principal========|")  
-        menu0()
+        print("\n",menu0.Menu1[1],"\n",menu.Menu1[2],"\n",menu.Menu1[3])
         print("|=======Puedes Elegir Otra Opcion : ========|")
     elif Menu == 2:
-        print("|=======Abriste Menu Valores========|")
-        menu0()
+        print("|=======Abriste Menu Personas========|")
+        print("\n",menu0.Menu1[0],"\n",menu.Menu1[2],"\n",menu.Menu1[3])
         print("|=======Puedes Elegir Otra Opcion : ========|")
     elif Menu == 3:
-        print("|=======Abriste Menu Personas========|")
-        menu0()
+        print("|=======Abriste Menu Clientes========|")
+        print("\n",menu0.Menu1[0],"\n",menu.Menu1[1],"\n",menu.Menu1[3])
         print("|=======Puedes Elegir Otra Opcion : ========|")
+    elif Menu == 4:
+        print("Gracias Vuelva Pronto")
     Menu = int(input("Digitar un valor : "))
 
