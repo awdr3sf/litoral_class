0352084423edac21da3911548e338b8e21cd8a05
awdr3sf <87214633+awdr3sf@users.noreply.github.com>
2022-04-06 23:37:43 -0500

    For

---
+++ Ejercicios en Clase/Python/Andres Camacho/ValorMaximoFor.py
@@ -0,0 +1,9 @@
+w = []
+for x in range (1,6):
+    x = int(input("Digitar Numeros: "))
+    print("|==================|")
+    w.append(x)
+    y = max(w)
+print("|===================================================|")
+print("| El valor maximo de los numeros digitados es: ", "|",y,"|")
+print("|===================================================|")
